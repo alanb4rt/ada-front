404e2b03ff11050b6cc2926b8baf1d7ad4f58c88
Antoine Schmerber-Perraud <antoine.schmerber-perraud@edu.devinci.fr>
2025-08-12 15:29:07 +0200

    Implements authentication context and logic

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./routes.tsx";
-import { ConfigProvider, theme } from "antd";
+import { ConfigProvider } from "antd";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -26,7 +27,9 @@ createRoot(document.getElementById("root")!).render(
         },
       }}
     >
-      <Router />
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
     </ConfigProvider>
   </StrictMode>
 );
