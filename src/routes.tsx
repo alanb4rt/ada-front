dc9a6817856dfc3620d1bd1f7bcd75d242e3177d
Alan Bart <al4n.b4rt@gmail.com>
2025-08-12 14:27:49 +0200

    feat: create dashboard page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App";
+import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
   },
 ]);
 
