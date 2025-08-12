918ca1820c5a714159b1dcb685a47d39eee1882c
Antoine Schmerber-Perraud <antoine.schmerber-perraud@edu.devinci.fr>
2025-08-12 14:46:14 +0200

    Improves login value handling

--- src/rules.ts
+++ src/rules.ts
@@ -1,7 +1,7 @@
 import type { Rule } from 'antd/es/form'
 
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-const phonePattern = /^(\+33|0)[1-9](\d{2}){4}$/
+const phonePattern = /^\d{10}$/
 
 export const emailRule = function (): Rule {
   return {
