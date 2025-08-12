e81d1cc466f01b06c05ce18e7d26f4b34b219278
Alan Bart <al4n.b4rt@gmail.com>
2025-08-12 14:38:45 +0200

    refactor: move Dashboard to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
-import { DatePicker } from 'antd'
+import { Layout } from "antd";
+import SideBar from "./components/SideBar";
+import ContentMessage from "./components/ContentMessage";
+
+const { Content, Sider } = Layout;
+
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: "center",
+  backgroundColor: "#1E1D1F",
+};
+
+const layoutStyle = {
+  overflow: "hidden",
+  height: "100vh",
+};
 
 export default function App() {
-  return <DatePicker />
+  return (
+    <>
+      <Layout style={layoutStyle}>
+        <Sider width="20%" style={siderStyle}>
+          <SideBar />
+        </Sider>
+        <Layout>
+          <Content style={contentStyle}>
+            <ContentMessage />
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
 }
