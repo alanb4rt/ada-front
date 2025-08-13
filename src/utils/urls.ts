4e4738c2aeb021373ec6e4ff46a8ecd8d3cc8cbc
Antoine Schmerber-Perraud <antoine.schmerber-perraud@edu.devinci.fr>
2025-08-13 10:32:35 +0200

    Renames login URL to authentication URL

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const USERS_URL = `${API_URL}/users`;
-export const LOGIN_URL = `${API_URL}/auth/login`;
+export const AUTH_URL = `${API_URL}/auth`;
