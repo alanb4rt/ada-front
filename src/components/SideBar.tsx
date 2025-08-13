45f9eb731370e3776f585809247fb687b96082fb
Alan Bart <al4n.b4rt@gmail.com>
2025-08-13 14:22:19 +0200

    feat: add second drawer

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,14 @@
-import { ArrowLeftOutlined, PlusCircleOutlined } from '@ant-design/icons'
-import { Button, Drawer, Flex, Space, Typography } from 'antd'
+import {
+  ArrowLeftOutlined,
+  PlusCircleOutlined,
+  UserAddOutlined,
+} from '@ant-design/icons'
+import { Avatar, Button, Drawer, Flex, Space, Typography } from 'antd'
 import { useState } from 'react'
 import { useAuth } from '../context/AuthContext'
 import UserCard from './UserCard'
 
-const { Title } = Typography
+const { Text, Title } = Typography
 
 const users = [
   { id: 1, name: 'Test', href: 'test' },
@@ -14,10 +18,15 @@ const users = [
   { id: 5, name: 'Test', href: 'test' },
 ]
 
+const drawerBodyStyle: React.CSSProperties = {
+  paddingInline: 0,
+}
+
 export default function SideBar() {
   const { logout } = useAuth()
 
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState<boolean>(false)
+  const [openGroup, setOpenGroup] = useState<boolean>(false)
 
   return (
     <Flex vertical style={{ height: '100vh' }}>
@@ -31,6 +40,39 @@ export default function SideBar() {
         open={open}
         mask={false}
         maskClosable={false}
+        styles={{ body: drawerBodyStyle }}
+      >
+        <Space
+          direction="vertical"
+          size={16}
+          style={{ width: '100%', flex: 1, overflowY: 'scroll' }}
+        >
+          <Space
+            style={{ padding: 16, cursor: 'pointer' }}
+            onClick={() => setOpenGroup(true)}
+          >
+            <Avatar
+              size={50}
+              icon={<UserAddOutlined />}
+              style={{ backgroundColor: '#b74040' }}
+            />
+            <Text>New group</Text>
+          </Space>
+          {users.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </Space>
+      </Drawer>
+      <Drawer
+        title="Add members to group"
+        closeIcon={<ArrowLeftOutlined />}
+        placement="left"
+        width={255}
+        onClose={() => setOpenGroup(false)}
+        open={openGroup}
+        mask={false}
+        maskClosable={false}
+        styles={{ body: drawerBodyStyle }}
       >
         <Space
           direction="vertical"
